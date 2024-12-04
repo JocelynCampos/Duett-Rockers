0b000cbf84dad306c3f4cf60f055f27cc2b7f9ad
ErikEdman96 <61149651+ErikEdman1996@users.noreply.github.com>
2024-12-04 20:04:03 +0100

    Gjort färdigt Game-klassen och påbörjat med CRUD-operationerna för GameDAO.

--- src/main/java/org/example/duetrockers/DAO/GameDAO.java
+++ src/main/java/org/example/duetrockers/DAO/GameDAO.java
@@ -1,4 +1,55 @@
 package org.example.duetrockers.DAO;
 
-public class GameDAO {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.example.duetrockers.entities.Game;
+
+public class GameDAO
+{
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("myconfig");
+    //CRUD operations
+
+    //CREATE
+    public boolean saveGame(Game game)
+    {
+
+        boolean result = false;
+
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try
+        {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(game);
+            transaction.commit();
+            result = true;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+
+            if(em != null && transaction != null && transaction.isActive())
+            {
+                transaction.rollback();
+            }
+        }
+        finally
+        {
+            em.close();
+        }
+
+        return result;
+    }
+
+
+    //READ One or ALL
+
+    //UPDATE
+
+    //DELETE
 }
