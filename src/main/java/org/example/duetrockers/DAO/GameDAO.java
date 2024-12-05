02eecf7ba6442dd56f6f7f7677a7243a15cd4c71
ErikEdman96 <61149651+ErikEdman1996@users.noreply.github.com>
2024-12-05 14:00:05 +0100

    Skrev färdigt GameDAO, gjorde flera tester i HelloApplication.

--- src/main/java/org/example/duetrockers/DAO/GameDAO.java
+++ src/main/java/org/example/duetrockers/DAO/GameDAO.java
@@ -1,10 +1,10 @@
 package org.example.duetrockers.DAO;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 import org.example.duetrockers.entities.Game;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameDAO
 {
@@ -46,10 +46,87 @@ public class GameDAO
         return result;
     }
 
-
     //READ One or ALL
+    public Game getGameByID(int id)
+    {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        Game gameToReturn = em.find(Game.class, id);
+        em.close();
+        return gameToReturn;
+    }
+
+    public List<Game> getAllGames()
+    {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        List<Game> listToReturn = new ArrayList<>();
+        TypedQuery<Game> query = em.createQuery("FROM Game", Game.class);
+        listToReturn.addAll(query.getResultList());
+        return listToReturn;
+    }
 
     //UPDATE
+    public boolean updateGame(Game gameToUpdate)
+    {
+        boolean result = false;
+
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try
+        {
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(em.contains(gameToUpdate) ? gameToUpdate : em.merge(gameToUpdate));
+            transaction.commit();
+            result = true;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            if(em != null && transaction != null && transaction.isActive())
+            {
+                transaction.rollback();
+            }
+        }
+        finally
+        {
+            em.close();
+        }
+
+        return result;
+    }
 
     //DELETE
+    public boolean deleteGame(Game gameToDelete)
+    {
+        boolean result = false;
+
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try
+        {
+            transaction = em.getTransaction();
+            transaction.begin();
+            if(!em.contains(gameToDelete))
+            {
+                gameToDelete = em.merge(gameToDelete);
+            }
+            em.remove(gameToDelete);
+            transaction.commit();
+            result = true;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            if(em != null && transaction != null && transaction.isActive())
+            {
+                transaction.rollback();
+            }
+        }
+        finally
+        {
+            em.close();
+        }
+
+        return result;
+    }
 }
--- src/main/java/org/example/duetrockers/HelloApplication.java
+++ src/main/java/org/example/duetrockers/HelloApplication.java
@@ -8,6 +8,7 @@ import org.example.duetrockers.DAO.GameDAO;
 import org.example.duetrockers.entities.Game;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HelloApplication extends Application {
     @Override
@@ -25,6 +26,50 @@ public class HelloApplication extends Application {
         {
             System.out.println("Game not saved");
         }
+
+        Game testGame = dao.getGameByID(1);
+
+        System.out.println("Game fetched: " +testGame.getGameName()+"\nId: "+testGame.getId());
+
+        Game testGame2 = new Game("League of Legends", 10, 10);
+
+        if(dao.saveGame(testGame2))
+        {
+            System.out.println("Game saved");
+        }
+        else
+        {
+            System.out.println("Game not saved");
+        }
+
+        List<Game> games = dao.getAllGames();
+
+        for(Game g : games)
+        {
+            System.out.println("Game name: "+g.getGameName()+"\nId: "+g.getId());
+        }
+
+        game.setMaxPlayers(3);
+
+       if(dao.updateGame(game))
+       {
+           System.out.println("Game updated");
+       }
+
+        System.out.println("Max players: "+game.getMaxPlayers());
+
+       if(dao.deleteGame(game))
+       {
+           System.out.println("Game deleted");
+       }
+
+        games = dao.getAllGames();
+
+        for(Game g : games)
+        {
+            System.out.println("Game name: "+g.getGameName()+"\nId: "+g.getId());
+        }
+
 
         AnchorPane root = new AnchorPane();
         Scene scene = new Scene(root, 320, 240);
