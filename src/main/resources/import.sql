70993bee73c688928326448f0357654c2b05cae8
Jocelyn <aracely.campos@hotmail.com>
2024-12-11 21:33:34 +0100

    Uppdaterat PersonDAO

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+-- Startdata
+
+INSERT INTO Person (first_name, last_name, nickname, street, postal_code, city, country, email, role)
+VALUES
+('Kamran','Akbari','K-man','Glaciärvägen 15','80621','Malung','Sweden','akbarikamran4@gmail.com','Staff'),
+('Erik','Edman','Rikki','Sicksackvägen 13','40232','Gävle','Sweden','erik.edman1996@gmail.com','Staff'),
+('Hugo','Ransvi','Huggster','Blåbärsvägen 2','80212','Norrköping','Sweden','ho.ransvi@gmail.com','Staff'),
+('Jocleyn','Campos','Jocy','Köpmangatan 42','60452','Göteborg','Sweden','aracely.campos@hotmail.com','Staff'),
+('Michael','Granbäck','Micke','Staketgatan 44','30150','Haparanda','Sweden','michael.granbäck@edugrade.se','Player'),
+('Jessica','Niord','Jessie','Kungsgatan 40','86392','Uddevalla','Sweden','jessica.niord@edugrade.se','Player');
