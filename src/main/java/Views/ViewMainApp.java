8804415f60114f2f0f4a19f86624c19a6cef4081
ErikEdman96 <61149651+ErikEdman1996@users.noreply.github.com>
2024-12-12 20:17:59 +0100

    Påbörjat ViewMainApp och testat hur det kan se ut när det är färdigt.

--- src/main/java/Views/ViewMainApp.java
+++ src/main/java/Views/ViewMainApp.java
@@ -3,7 +3,14 @@ package Views;
 import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.text.Font;
+import org.example.duetrockers.DAO.PersonDAO;
+import org.example.duetrockers.DAO.StaffDAO;
+import org.example.duetrockers.entities.Person;
+import org.example.duetrockers.entities.Staff;
+
+import java.util.List;
 
 public class ViewMainApp extends View
 {
@@ -11,9 +18,13 @@ public class ViewMainApp extends View
     private static final int BUTTON_HEIGHT = 60;
 
 
-    private Button add, update, returnButton, exit;
+    private Button add, update, returnButton, exit, delete;
 
     private Label staffLabel;
+
+    private ListView<String> tableList;
+
+    private ListView<String> dataList;
 
     public ViewMainApp(int width, int height, ViewManager manager)
     {
@@ -27,23 +38,28 @@ public class ViewMainApp extends View
         update = new Button("Update");
         returnButton = new Button("Return");
         exit = new Button("Exit");
+        delete = new Button("Delete");
 
         add.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         update.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         returnButton.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         exit.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        delete.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 
-        add.setLayoutX(width / 2 - add.getPrefWidth() / 2);
-        add.setLayoutY((height / 4 - add.getPrefHeight() / 2) + BUTTON_HEIGHT);
+        add.setLayoutX((width / 2 - add.getPrefWidth() / 2) + 150);
+        add.setLayoutY((height / 5 - add.getPrefHeight() / 2) + BUTTON_HEIGHT);
 
-        update.setLayoutX(width / 2 - update.getPrefWidth() / 2);
-        update.setLayoutY((height / 4 - update.getPrefHeight() / 2) + (BUTTON_HEIGHT * 2) + 10);
+        update.setLayoutX((width / 2 - update.getPrefWidth() / 2)+ 150);
+        update.setLayoutY((height / 5 - update.getPrefHeight() / 2) + (BUTTON_HEIGHT * 2) + 10);
 
-        returnButton.setLayoutX(width / 2 - returnButton.getPrefWidth() / 2);
-        returnButton.setLayoutY((height / 4 - returnButton.getPrefHeight() / 2) + (BUTTON_HEIGHT * 3) + 20);
+        delete.setLayoutX((width / 2 - delete.getPrefWidth() / 2)+ 150);
+        delete.setLayoutY((height / 5 - delete.getPrefHeight() / 2) + (BUTTON_HEIGHT*3) + 20);
 
-        exit.setLayoutX(width / 2 - exit.getPrefWidth() / 2);
-        exit.setLayoutY((height / 4 - exit.getPrefHeight() / 2) + (BUTTON_HEIGHT * 4) + 30);
+        returnButton.setLayoutX((width / 2 - returnButton.getPrefWidth() / 2)+ 150);
+        returnButton.setLayoutY((height / 5 - returnButton.getPrefHeight() / 2) + (BUTTON_HEIGHT * 4) + 30);
+
+        exit.setLayoutX((width / 2 - exit.getPrefWidth() / 2)+ 150);
+        exit.setLayoutY((height / 5 - exit.getPrefHeight() / 2) + (BUTTON_HEIGHT * 5) + 40);
 
         returnButton.setOnAction(e -> {
            manager.switchToPreviousView();
@@ -53,14 +69,83 @@ public class ViewMainApp extends View
             Platform.exit();
         });
 
-        staffLabel = new Label(manager.getCurrentStaff());
+        staffLabel = new Label("Current staff: "+ manager.getCurrentStaff());
 
         staffLabel.setFont(new Font(20));
 
         staffLabel.setLayoutX(width / 8 - staffLabel.getPrefWidth() / 2);
         staffLabel.setLayoutY((height / 8 - staffLabel.getPrefHeight() / 2) - 30);
 
-        root.getChildren().addAll(add, update, returnButton, exit, staffLabel);
+        tableList = new ListView();
+
+        tableList.setLayoutX(width / 8 - tableList.getPrefWidth() / 2);
+        tableList.setLayoutY(height / 8 - tableList.getPrefHeight() / 2);
+
+        tableList.getItems().addAll("Persons");
+        tableList.getItems().addAll("Staff");
+        tableList.getItems().addAll("Games");
+        tableList.getItems().addAll("Players");
+        tableList.getItems().addAll("Teams");
+        tableList.getItems().addAll("Team vs Team-matches");
+        tableList.getItems().addAll("Player vs Player-matches");
+
+        tableList.setPrefHeight(tableList.getItems().size() * 24 + 2);
+
+        dataList = new ListView();
+
+        dataList.setLayoutX(tableList.getLayoutX());
+        dataList.setLayoutY(tableList.getLayoutY() + 200);
+
+        tableList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            if(newValue != null)
+            {
+                dataList.getItems().clear();
+
+                switch(newValue)
+                {
+                    case "Persons":
+                        addPersonsToDataList();
+                        break;
+                    case "Staff":
+                        addStaffToDataList();
+                        break;
+                }
+            }
+
+        });
+
+
+
+        dataList.setPrefHeight(dataList.getItems().size() * 24 + 2);
+
+        root.getChildren().addAll(add, update, returnButton, exit, staffLabel, delete, tableList, dataList);
+    }
+
+    private void addPersonsToDataList()
+    {
+        PersonDAO personDAO = new PersonDAO();
+        List<Person> persons = personDAO.getAllPersons();
+
+        for(Person person : persons)
+        {
+            dataList.getItems().add(person.getFirstName() + " " + person.getLastName());
+        }
+
+        dataList.setPrefHeight(dataList.getItems().size() * 24 + 2);
+    }
+
+    private void addStaffToDataList()
+    {
+        StaffDAO staffDAO = new StaffDAO();
+        List<Staff> staffList = staffDAO.getAllStaff();
+
+        for(Staff staff : staffList)
+        {
+            dataList.getItems().add(staff.getPerson().getFirstName() + " " + staff.getPerson().getLastName());
+        }
+
+        dataList.setPrefHeight(dataList.getItems().size() * 24 + 2);
     }
 
 }
