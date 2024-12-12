64b9ebb1a8668385dd144615d94232e7e6dd11c0
Jocelyn <aracely.campos@hotmail.com>
2024-12-12 15:32:25 +0100

    Uppdaterat PersonDAO

--- src/main/java/org/example/duetrockers/DAO/PersonDAO.java
+++ src/main/java/org/example/duetrockers/DAO/PersonDAO.java
@@ -1,8 +1,11 @@
 package org.example.duetrockers.DAO;
 
+import jakarta.persistence.criteria.From;
 import org.example.duetrockers.entities.Person;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class PersonDAO {
     private Session session;
@@ -16,8 +19,19 @@ public class PersonDAO {
     }
 
     public void showPersons () {
-        Transaction transaction = session.beginTransaction();
-        //session.
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            List<Person> persons = session.createQuery("FROM Person", Person.class).list();
+            for (Person person : persons) {
+                System.out.println(person); //Skriv ut varje person
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     public void savePerson (Person person) {
@@ -29,7 +43,7 @@ public class PersonDAO {
 
     }
 
-    public void deletePerson (int id) {
+    public void removePerson (int id) {
         Transaction transaction = session.beginTransaction();
         Person person = session.get(Person.class, id);
         if (person != null) {
