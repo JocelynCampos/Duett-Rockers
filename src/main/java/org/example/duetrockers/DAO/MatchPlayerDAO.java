4d88f4e7479a1837c58ea1772d00366982b06d33
Jocelyn <aracely.campos@hotmail.com>
2024-12-13 12:43:57 +0100

    Uppdaterat MatchTeamDao

--- src/main/java/org/example/duetrockers/DAO/MatchPlayerDAO.java
+++ src/main/java/org/example/duetrockers/DAO/MatchPlayerDAO.java
@@ -1,4 +1,20 @@
 package org.example.duetrockers.DAO;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class MatchPlayerDAO {
+    //CRUD - operations
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("myconfig");
+
+    //Create
+
+
+    //Read
+
+    //Update
+
+    //Delete
 }
--- src/main/java/org/example/duetrockers/DAO/MatchTeamDAO.java
+++ src/main/java/org/example/duetrockers/DAO/MatchTeamDAO.java
@@ -1,4 +1,94 @@
 package org.example.duetrockers.DAO;
 
+import jakarta.persistence.*;
+import org.example.duetrockers.entities.MatchTeam;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatchTeamDAO {
+    //CRUD - operations
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("myconfig");
+
+    //Create
+    public boolean saveMatchTeam (MatchTeam matchTeam) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(matchTeam);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (entityManager != null && transaction!= null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            return false;
+        } finally {
+                entityManager.close();
+        }
+    }
+
+    //Read ONE
+    public MatchTeam getMatchTeamById (int id) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        MatchTeam matchTeamListToReturn = entityManager.find(MatchTeam.class, id);
+        entityManager.close();
+        return matchTeamListToReturn;
+    }
+     //Read ALL
+    public List <MatchTeam> getAllMatchTeams () {
+        try (EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager()) {
+            return entityManager.createQuery("FROM MatchTeam", MatchTeam.class).getResultList();
+        } catch (Exception e) {
+            System.out.println("Error when getting all teams..");
+            return new ArrayList<>();
+        }
+    }
+
+    //Update
+    public boolean updateMatchTeam (MatchTeam matchTeam) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.merge(matchTeam);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            return false;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    //Delete
+    public boolean deleteMatchTeam (MatchTeam matchTeam) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            MatchTeam matchTeam1;
+            entityManager.remove(matchTeam);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+
+                transaction.rollback();
+                System.out.println(e.getMessage());
+            } return false;
+        } finally {
+            entityManager.close();
+        }
+    }
 }
