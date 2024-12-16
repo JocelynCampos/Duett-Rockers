0b621ee8f07becc9dbe9bd3a1c9a6344de2e4eff
Jocelyn <aracely.campos@hotmail.com>
2024-12-16 21:44:03 +0100

    ViewTeamMatches

--- src/main/java/Views/ViewTeamMatches.java
+++ src/main/java/Views/ViewTeamMatches.java
@@ -9,7 +9,10 @@ import org.example.duetrockers.DAO.TeamDAO;
 import org.example.duetrockers.entities.MatchTeam;
 import org.example.duetrockers.entities.Team;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ViewTeamMatches extends View {
 
@@ -20,6 +23,7 @@ public class ViewTeamMatches extends View {
     private TextField searchField;
     private Button selectTeam1Button, getSelectTeam2Button;
     private Team Team1, Team2;
+    private Map<String, Integer> matchIdMap = new HashMap<>();
 
     private MatchTeamDAO matchTeamDAO;
     private TeamDAO teamDAO;
@@ -33,56 +37,36 @@ public class ViewTeamMatches extends View {
     protected void initializeView() {
         root = new AnchorPane();
 
-
         matchTeamDAO = new MatchTeamDAO();
         teamDAO = new TeamDAO();
 
+        //Skapa titel
         Label titleLabel = new Label("Team Matches");
         titleLabel.setFont(new Font(24));
 
+        //Knappar
         addButton = new Button("Add");
         updateButton = new Button("Update");
         deleteButton = new Button("Delete");
         returnButton = new Button("Return");
+
+        //ListViews
+        availableTeamsListView = new ListView<>();
         teamListView = new ListView<>();
+
+        //Ladda data
+        loadAvailableTeams();
+
+        selectTeam();
+
         loadTeamMatches();
 
 
         HBox buttonBox = new HBox(10, addButton, updateButton, deleteButton, returnButton);
 
-
-        addButton.setOnAction(e -> {
-            if (Team1 != null && Team2 != null) {
-                MatchTeam newMatch = new MatchTeam();
-                newMatch.setTeam1(Team1);
-                newMatch.setTeam2(Team2);
-                matchTeamDAO.addMatchTeam(newMatch);
-
-                teamListView.getItems().add(Team1.getTeamName() + " vs " + Team2.getTeamName());
-                Team1 = null;
-                Team2 = null;
-                selectedTeamsListView.getItems().clear();
-            }
-
-        });
-        updateButton.setOnAction(e -> {
-            String selectedMatch = teamListView.getSelectionModel().getSelectedItem();
-            if (selectedMatch != null) {
-
-            }
-        });
-
-
-
-
-        deleteButton.setOnAction(e -> {
-            String selectecMatch = teamListView.getSelectionModel().getSelectedItem();
-            if (selectecMatch != null) {
-                teamListView.getItems().remove(selectecMatch);
-            }
-        });
-
-
+        addButton.setOnAction(e -> handleAddMatch());
+        updateButton.setOnAction(e -> handleUpdateMatch());
+        deleteButton.setOnAction(e -> handleDeleteMatch());
         returnButton.setOnAction(e -> manager.switchToPreviousView());
 
 
@@ -99,12 +83,15 @@ public class ViewTeamMatches extends View {
 
     private void loadTeamMatches() {
         List<MatchTeam> matches = matchTeamDAO.getAllMatchTeams();
-
-
         for (MatchTeam match : matches) {
             String displayText = match.getTeam1().getTeamName() + " vs " + match.getTeam2().getTeamName();
             teamListView.getItems().add(displayText);
+            matchIdMap.put(displayText, match.getId());
         }
+    }
+
+    private int getMatchID(String matchText) {
+        return matchIdMap.getOrDefault(matchText, -1);
     }
 
     private void loadAvailableTeams() {
@@ -129,8 +116,68 @@ public class ViewTeamMatches extends View {
         });
     }
 
-    private void addMatchTeam() {
-        for ()
+    private void handleAddMatch() {
+        if (Team1 != null && Team2 != null) {
+            MatchTeam newMatch = new MatchTeam();
+            newMatch.setTeam1(Team1);
+            newMatch.setTeam2(Team2);
+            matchTeamDAO.saveMatchTeam(newMatch);
+
+            teamListView.getItems().add(Team1.getTeamName() + " vs " + Team2.getTeamName());
+            Team1 = null;
+            Team2 = null;
+            availableTeamsListView.getItems().remove(Team1.getTeamName());
+            availableTeamsListView.getItems().remove(Team2.getTeamName());
+            loadTeamMatches();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Match added!");
+            alert.setHeaderText(null);
+            alert.setContentText("Match between " + newMatch.getTeam1().getTeamName() + " and " + newMatch.getTeam2().getTeamName() + " has succsessfully been created!");
+            alert.showAndWait();
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error..");
+            alert.setHeaderText(null);
+            alert.setContentText("\"Choose two teams before creating a match!\"");
+            alert.showAndWait();
+        }
+    }
+
+    private void handleUpdateMatch() {
+        String selectedMatch = teamListView.getSelectionModel().getSelectedItem();
+        if (selectedMatch != null) {
+            MatchTeam match = matchTeamDAO.getMatchTeamById(getMatchID(selectedMatch));
+            match.setTeam1(Team1);
+            match.setTeam2(Team2);
+            matchTeamDAO.updateMatchTeam(match);
+            loadTeamMatches();
+            System.out.println("Updating team: " + selectedMatch);
+        }
+    }
+
+    private void handleDeleteMatch() {
+        String selectedMatch = teamListView.getSelectionModel().getSelectedItem();
+        if (selectedMatch != null) {
+            int matchId = getMatchID(selectedMatch);
+            if (matchId != 1) {
+                matchTeamDAO.deleteMatchTeam(matchId);
+                teamListView.getItems().remove(selectedMatch);
+                System.out.println("Match deleted: " + selectedMatch);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Could not find the selected match to delete");
+                alert.showAndWait();
+            }
+        } else { Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error.");
+            alert.setHeaderText(null);
+            alert.setContentText("No match selected to delete.");
+            alert.showAndWait();
+        }
     }
 }
 
